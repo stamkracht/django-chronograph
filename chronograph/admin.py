cd3bdb79c45e9843e2bcc2788b5f98bbcc9185c2
Tim Graham <tim@westbrook.(none)>
2011-03-22 13:48:36 -0400

    `info_subscribers` should be a raw_id_field to prevent browser crash with a large list of users.

--- chronograph/admin.py
+++ chronograph/admin.py
@@ -19,14 +19,14 @@ class HTMLWidget(forms.Widget):
     def __init__(self,rel=None, attrs=None):
         self.rel = rel
         super(HTMLWidget, self).__init__(attrs)
-    
+
     def render(self, name, value, attrs=None):
         if self.rel is not None:
             key = self.rel.get_related_field().name
             obj = self.rel.to._default_manager.get(**{key: value})
             related_url = '../../../%s/%s/%d/' % (self.rel.to._meta.app_label, self.rel.to._meta.object_name.lower(), value)
             value = "<a href='%s'>%s</a>" % (related_url, escape(obj))
-            
+
         final_attrs = self.build_attrs(attrs, name=name)
         return mark_safe("<div%s>%s</div>" % (flatatt(final_attrs), linebreaks(value)))
 
@@ -62,11 +62,10 @@ class JobAdmin(admin.ModelAdmin):
     )
     list_display_links = ('name', )
     list_filter = ('last_run_successful', 'frequency', 'disabled')
-    filter_horizontal = ('info_subscribers', 'subscribers',)
     search_fields = ('name', )
     ordering = ('last_run', )
-    raw_id_fields = ('subscribers',)
-    
+    raw_id_fields = ('subscribers', 'info_subscribers')
+
     fieldsets = (
         (_('Job Details'), {
             'classes': ('wide',),
@@ -81,42 +80,42 @@ class JobAdmin(admin.ModelAdmin):
             'fields': ('frequency', 'next_run', 'params',)
         }),
     )
-    
+
     def last_run_with_link(self, obj):
         format = get_date_formats()[1]
         value = capfirst(dateformat.format(obj.last_run, format))
-        
+
         log_id = obj.log_set.latest('run_date').id
-        
+
         try:
             # Old way
             reversed_url = reverse('chronograph_log_change', args=(log_id,))
         except:
             # New way
             reversed_url = reverse('admin:chronograph_log_change', args=(log_id,))
-        
+
         return '<a href="%s">%s</a>' % (reversed_url, value)
     last_run_with_link.allow_tags = True
     last_run_with_link.short_description = _('Last run')
     last_run_with_link.admin_order_field = 'last_run'
-    
+
     def job_success(self, obj):
         return obj.last_run_successful
     job_success.short_description = _(u'OK')
     job_success.boolean = True
-    
+
     def run_button(self, obj):
         on_click = "window.location='%d/run/?inline=1';" % obj.id
         return '<input type="button" onclick="%s" value="Run" />' % on_click
     run_button.allow_tags = True
     run_button.short_description = _('Run')
-    
+
     def view_logs_button(self, obj):
         on_click = "window.location='../log/?job=%d';" % obj.id
         return '<input type="button" onclick="%s" value="View Logs" />' % on_click
     view_logs_button.allow_tags = True
     view_logs_button.short_description = _('Logs')
-    
+
     def run_job_view(self, request, pk):
         """
         Runs the specified job.
@@ -127,14 +126,14 @@ class JobAdmin(admin.ModelAdmin):
             raise Http404
         job.run(save=False)
         request.user.message_set.create(message=_('The job "%(job)s" was run successfully.') % {'job': job})
-        
+
         if 'inline' in request.GET:
             redirect = request.path + '../../'
         else:
             redirect = request.REQUEST.get('next', request.path + "../")
-        
+
         return HttpResponseRedirect(redirect)
-    
+
     def get_urls(self):
         urls = super(JobAdmin, self).get_urls()
         my_urls = patterns('',
@@ -154,7 +153,7 @@ class LogAdmin(admin.ModelAdmin):
             'fields': ('stdout', 'stderr',)
         }),
     )
-    
+
     def job_duration(self, obj):
         return "%s" % (obj.get_duration())
     job_duration.short_description = _(u'Duration')
@@ -162,40 +161,40 @@ class LogAdmin(admin.ModelAdmin):
     def job_name(self, obj):
         return obj.job.name
     job_name.short_description = _(u'Name')
-    
+
     def job_success(self, obj):
         return obj.success
     job_success.short_description = _(u'OK')
     job_success.boolean = True
-    
+
     def output(self, obj):
         result = obj.stdout or ''
         if len(result) > 40:
             result = result[:40] + '...'
-        
+
         return result or _('(No output)')
-    
+
     def errors(self, obj):
         result = obj.stderr or ''
         if len(result) > 40:
             result = result[:40] + '...'
-        
+
         return result or _('(No errors)')
-    
+
     def has_add_permission(self, request):
         return False
-    
+
     def formfield_for_dbfield(self, db_field, **kwargs):
         request = kwargs.pop("request", None)
 
         if isinstance(db_field, models.TextField):
             kwargs['widget'] = HTMLWidget()
             return db_field.formfield(**kwargs)
-        
+
         if isinstance(db_field, models.ForeignKey):
             kwargs['widget'] = HTMLWidget(db_field.rel)
             return db_field.formfield(**kwargs)
-        
+
         return super(LogAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(Job, JobAdmin)
