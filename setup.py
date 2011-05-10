dd0dcdd0a9bd41b682c5fbf13abd32b4df8872d2
Tim Graham <timgraham@google.com>
2011-05-10 12:55:20 -0400

    Moved error output before info output in subscribers email.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ app_name = 'django-chronograph'
 
 setup(
     name=app_name,
-    version='0.1.9',
+    version='0.1.10',
     description='Django chronograph application.',
     author='Weston Nielson',
     author_email='wnielson@gmail.com',
