ec8df07e8ed007aeb401e29688a5b15820fc5724
Alex Olieman <alex@olieman.net>
2016-05-09 14:00:03 +0200

    pep8 improvements

--- chronograph/management/commands/cron_clean.py
+++ chronograph/management/commands/cron_clean.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand
 
 
-class Command( BaseCommand ):
+class Command(BaseCommand):
     help = 'Deletes old job logs.'
 
     def add_arguments(self, parser):
         parser.add_argument('unit', choices=['weeks', 'days', 'hours', 'minutes'])
         parser.add_argument('interval', type=int)
 
-    def handle( self, **options ):
+    def handle(self, **options):
         from chronograph.models import Log
         from datetime import datetime, timedelta
         kwargs = {options['unit']: options['interval']}
         time_ago = datetime.now() - timedelta(**kwargs)
-        Log.objects.filter(run_date__lte = time_ago).delete()
+        Log.objects.filter(run_date__lte=time_ago).delete()
